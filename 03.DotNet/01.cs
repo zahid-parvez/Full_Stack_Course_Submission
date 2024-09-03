984dd023ae8fada5ac8f2bbe2254597a61e95f44
Zahid Parvez <91465009+zahid-parvez@users.noreply.github.com>
2024-09-03 17:59:50 +0530

    Add files via upload

---
+++ 03.DotNet/01.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1.Read Employee details from the user and display on console. 
+//		Employee Id :  6546545
+
+//        Employee Name   :  Scott
+//        Employee Job :   Manager
+        //Employee Salary :   85000
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string email, name;
+            int age;
+            Console.WriteLine("Hi, I'm Zahid");
+            Console.WriteLine("Enter name :");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter email:");
+            email = Console.ReadLine();
+            Console.WriteLine("Enter age");
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\t------User Details -------");
+            Console.WriteLine("Name : " + name);
+            Console.WriteLine("Email : " + email);
+            Console.WriteLine("Age : " + age);
+            Console.Read();
+        }
+    }
+}
---
+++ 03.DotNet/assignmet1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1.Read Employee details from the user and display on console. 
+//		Employee Id :  6546545
+
+//        Employee Name   :  Scott
+//        Employee Job :   Manager
+        //Employee Salary :   85000
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string id, name, job;
+            int salary;
+            Console.WriteLine("Enter Emp Id :");
+            id = Console.ReadLine();
+            Console.WriteLine("Enter name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Job:");
+            job = Console.ReadLine();
+            Console.WriteLine("Enter Salary: ");
+            salary = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\t------User Details -------");
+            Console.WriteLine("Employee Id : " + id);
+            Console.WriteLine("Employee name : " + name);
+            Console.WriteLine("Employee Job : " + job);
+            Console.WriteLine("Emplyee Salary : " + salary);
+            Console.Read();
+        }
+    }
+}
