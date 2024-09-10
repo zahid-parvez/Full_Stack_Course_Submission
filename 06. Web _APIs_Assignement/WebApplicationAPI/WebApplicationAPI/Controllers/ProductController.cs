31f24136446b1e9093ff083e5b967c42781036fb
Zahid Parvez <91465009+zahid-parvez@users.noreply.github.com>
2024-09-10 16:56:57 +0530

    Add files via upload
    ASP.NET Web API assignment - Sep 10, 2024

---
+++ 06. Web _APIs_Assignement/WebApplicationAPI/WebApplicationAPI/Controllers/ProductController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationAPI.Models;
+
+namespace WebApplicationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public ProductDbContext _context;
+
+        public ProductController(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            List<Product> obj = _context.Products.ToList();
+            return Ok(obj);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductsById(int id)
+        {
+            Product obj = _context.Products.Find(id);
+            if(obj != null)
+            {
+                return Ok(obj);
+            }
+            else
+            {
+                return NotFound(new { Status = "Product Details Not Found" });
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult AddProduct(Product obj)
+        {
+            _context.Products.Add(obj);
+            _context.SaveChanges();
+            return Ok(obj);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProduct(int id)
+        {
+            Product obj = _context.Products.Find(id);
+            if(obj != null)
+            {
+                _context.Products.Remove(obj);
+                _context.SaveChanges();
+                return Ok(new { Staus = "Product Deletes Successfully !" });
+            }
+            else
+            {
+                return NotFound(new { Status = "Product could not be deleated as no record Found" });
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult EditProduct(Product obj)
+        {
+            if(obj != null)
+
+           { _context.Products.Update(obj);
+            _context.SaveChanges();
+            return Ok(obj);
+            }
+
+            return NotFound(new { status = "Could not be updated" });
+        }
+    }
+}
